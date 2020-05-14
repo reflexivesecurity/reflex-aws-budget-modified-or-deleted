fa0f7c50ae46ec5713904fd0c44d43f0e3d114b8
Chris Coleman <chris@rot-26.com>
2020-05-13 20:56:55 -0600

    cleanup modules

--- terraform/cwe/main.tf
+++ terraform/cwe/main.tf
@@ -1,4 +1,4 @@
-module "reflex_aws_budget_modified_or_deleted" {
+module "cwe" {
   source           = "git::https://github.com/cloudmitigator/reflex-engine.git//modules/cwe_lambda?ref=v0.5.7"
   rule_name        = "BudgetModifiedOrDeleted"
   rule_description = "A Reflex Rule for detecting the modification or deletion of an AWS Budget"
@@ -23,19 +23,4 @@ module "reflex_aws_budget_modified_or_deleted" {
 }
 PATTERN
 
-  function_name   = "BudgetModifiedOrDeleted"
-  source_code_dir = "${path.module}/source"
-  handler         = "reflex_aws_budget_modified_or_deleted.lambda_handler"
-  lambda_runtime  = "python3.7"
-  environment_variable_map = {
-    SNS_TOPIC = var.sns_topic_arn,
-  }
-
-  queue_name    = "BudgetModifiedOrDeleted"
-  delay_seconds = 0
-
-  target_id = "BudgetModifiedOrDeleted"
-
-  sns_topic_arn  = var.sns_topic_arn
-  sqs_kms_key_id = var.reflex_kms_key_id
 }
--- terraform/sqs_lambda/sqs_lambda.tf
+++ terraform/sqs_lambda/sqs_lambda.tf
@@ -1,28 +1,4 @@
-module "reflex_aws_budget_modified_or_deleted" {
-  source           = "git::https://github.com/cloudmitigator/reflex-engine.git//modules/cwe_lambda?ref=v0.5.7"
-  rule_name        = "BudgetModifiedOrDeleted"
-  rule_description = "A Reflex Rule for detecting the modification or deletion of an AWS Budget"
-
-  event_pattern = <<PATTERN
-{
-  "source": [
-    "aws.budgets"
-  ],
-  "detail-type": [
-    "AWS API Call via CloudTrail"
-  ],
-  "detail": {
-    "eventSource": [
-      "budgets.amazonaws.com"
-    ],
-    "eventName": [
-      "UpdateBudget",
-      "DeleteBudget"
-    ]
-  }
-}
-PATTERN
-
+module "sqs_lambda" {
   function_name   = "BudgetModifiedOrDeleted"
   source_code_dir = "${path.module}/source"
   handler         = "reflex_aws_budget_modified_or_deleted.lambda_handler"
